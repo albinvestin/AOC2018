077ee443d766614e2655e3f1668f8ab518416f98
Albin Vestin <abbe_westin@hotmail.com>
2019-08-16 08:48:14 +0200

    Updated for part 2 of day 3

--- day_3/main.py
+++ day_3/main.py
@@ -12,7 +12,19 @@ def fill_area(input_file):
 
             occupied_positions[int(x_position):int(x_position)+int(x_size), int(y_position):int(y_position)+int(y_size)] += 1
 
-        return np.size(np.where(occupied_positions >= 2)[0])
+        #return np.size(np.where(occupied_positions >= 2)[0])
+    # Part 2
+    with open(input_file, 'r') as read_line:
+    for line in read_line:
+        id, _, position, size = line.split()
+        id = int(id.strip('#'))
+        x_position, y_position = position[:-1].split(',')
+        x_size, y_size = size.split('x')
+
+        if np.any(occupied_positions[int(x_position):int(x_position)+int(x_size), int(y_position):int(y_position)+int(y_size)] > 1):
+            continue
+        else:
+            return id
 
 
 
