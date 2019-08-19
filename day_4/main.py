eeb3ee193d71d5d4c82df300327dbfa76089336a
Albin Vestin <abbe_westin@hotmail.com>
2019-08-19 08:53:16 +0200

    Update for Day 4 Part 2

--- day_4/main.py
+++ day_4/main.py
@@ -44,6 +44,19 @@ def get_most_sleeping_guard(input_file):
     return max(guard_sleep_count, key=lambda attribute: attribute[1])
 
 
+def get_all_guard_ids(input_file):
+    id = -1
+    all_guard_ids = []
+    for line in input_file:
+        action = line[5]
+        if action.find('#') != -1:
+            extract_id = parse.parse('Guard #{id:d}{}', action)
+            id = extract_id['id']
+            if id not in all_guard_ids:
+                all_guard_ids.append(id)
+    return all_guard_ids
+
+
 def is_asleep(action):
     return action.find('asleep') != -1
 
@@ -73,14 +86,30 @@ def get_most_slept_minute(input_file, guard_id):
                 minute = line[4]
                 slept_minutes[sleep_start:minute] += 1
 
-    return np.argmax(slept_minutes)
+    return [np.argmax(slept_minutes), np.max(slept_minutes)]
+
+
+def get_most_frequent_sleeping_guard(input_file, guard_ids):
+    # count = [id, minute number, total count]
+    most_frequent_guard_count = [[0, 0, 0]]
+    for id in guard_ids:
+        new_guard_count = get_most_slept_minute(input_file, id)
+        if most_frequent_guard_count[0][2] < new_guard_count[1]:
+            most_frequent_guard_count = [[id, new_guard_count[0], new_guard_count[1]]]
+    return most_frequent_guard_count
 
 
 if __name__ == '__main__':
     sorted_input = sort_input('input')
-    guard_id = get_most_sleeping_guard(sorted_input)
-    guard_minutes = get_most_slept_minute(sorted_input, guard_id[0])
+    #guard_id = get_most_sleeping_guard(sorted_input)
+    #guard_minutes = get_most_slept_minute(sorted_input, guard_id[0])
 
-    print('Guard ID: {}, Number of minutes slept: {}, Most slept minute: {}'.format(guard_id[0], guard_id[1], guard_minutes))
-    print(guard_id[0] * guard_minutes)
+    #print('Guard ID: {}, Number of minutes slept: {}, Most slept minute: {}'.format(guard_id[0], guard_id[1], guard_minutes))
+    #print(guard_id[0] * guard_minutes)
+
+    # Part 2
+    guard_ids = get_all_guard_ids(sorted_input)
+    most_frequent_sleeping_guard = get_most_frequent_sleeping_guard(sorted_input, guard_ids)
+    print(most_frequent_sleeping_guard)
+
 
